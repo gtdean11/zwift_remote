9743500e3e37304253f7010d5a98ff6f22404d78
Dean <gtdean11@gmail.com>
2025-08-05 11:35:11 -0400

    Initial update

--- remote.lua
+++ remote.lua
@@ -3,22 +3,31 @@ local kb = libs.keyboard;
 actions.powerup = function ()
 	kb.stroke("space");
 end
+
 actions.screenshot = function ()
 	kb.stroke("f10");
+end
+
+actions.elbowflick = function ()
+	kb.stroke("f1");
 end
 
 actions.camfront = function ()
 	kb.stroke("1");
 end
+
 actions.camrear = function ()
 	kb.stroke("6");
 end
+
 actions.camrear2s = function ()
 	kb.stroke("win","8");
 end
+
 actions.droneflyoverl = function ()
 	kb.stroke("win", "9");
 end
+
 actions.droneflyover2 = function ()
 	kb.stroke("win", "0");
 end
@@ -30,24 +39,35 @@ end
 actions.bike1 = function ()
 	kb.stroke("win", "1");
 end
+
 actions.bike2 = function ()
 	kb.stroke("win", "2");
 end
+
 actions.bike3 = function ()
 	kb.stroke("win", "3");
 end
+
 actions.bike4 = function ()
 	kb.stroke("win", "4");
 end
+
 actions.bike5 = function ()
 	kb.stroke("win", "5");
 end
+
 actions.bike6 = function ()
 	kb.stroke("win", "6");
 end
+
 actions.bike7 = function ()
 	kb.stroke("win", "7");
 end
+
+actions.bike8 = function ()
+	kb.stroke("ctrl","shift","y");
+end
+
 actions.pairing = function ()
 	kb.stroke("a");
 end
@@ -59,12 +79,19 @@ end
 actions.pmirt = function ()
 	kb.stroke("ctrl","shift","3");
 end
+
 actions.wdirt = function ()
 	kb.stroke("ctrl","shift","2");
 end
+
 actions.bdirt = function ()
 	kb.stroke("ctrl","shift","1");
 end
+
 actions.rbow = function ()
 	kb.stroke("ctrl","shift","4");
+end
+
+actions.flowers = function ()
+	kb.stroke("ctrl","shift","5p");
 end
